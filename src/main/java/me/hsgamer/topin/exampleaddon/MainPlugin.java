62fc8d592c45681262bf835f6f1a0a5133956080
HSGamer <huynhqtienvtag@gmail.com>
2020-08-30 17:15:26 +0700

    Initial commit

---
+++ src/main/java/me/hsgamer/topin/exampleaddon/MainPlugin.java
@@ -0,0 +1,22 @@
+package me.hsgamer.topin.exampleaddon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainPlugin extends JavaPlugin {
+
+  /**
+   * Called when enabling the addon
+   */
+  @Override
+  public void onEnable() {
+    // Enable logic
+  }
+
+  /**
+   * Called when disabling the addon
+   */
+  @Override
+  public void onDisable() {
+    // Disable logic
+  }
+}
